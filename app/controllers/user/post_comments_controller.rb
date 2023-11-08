4021fc9e86ac02fc8e4d41a28e8ede080efa0df0
19951118 <reonn1118@yahoo.co.jp>
2023-11-08 07:42:23 +0000

    イン、アップレイアウト等

--- app/controllers/user/post_comments_controller.rb
+++ app/controllers/user/post_comments_controller.rb
@@ -1,6 +1,6 @@
 class User::PostCommentsController < ApplicationController
   def create
-    @post = Post.find(params[:book_id])
+    @post = Post.find(params[:post_id])
     @comment = current_user.post_comments.new(post_comment_params)
     @comment.post_id = @post.id
     @comment.save
--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -32,7 +32,6 @@ class User::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @post_comment = PostComment.new
-    @post_comments = @post.post_comments
   end
 
  private
