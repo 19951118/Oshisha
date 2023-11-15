c98f41f72b61f24d61ccd32ef2d2cb8b94dfc587
19951118 <reonn1118@yahoo.co.jp>
2023-11-15 10:47:36 +0000

    top_image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,155 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(email: 'o@shisha', password: 'oshisha')
+  Admin.create!(email: 'o@shisha', password: 'oshisha')
+   
+  10.times do |num|
+  User.create!(
+    name: "シーシャ#{num + 1}本目",
+    email: "a#{num + 1}@a",
+    password: "aaaaaa#{num + 1}",
+    password_confirmation: "aaaaaa#{num + 1}",
+    profile: "シーシャは一日#{num + 1}本吸う",
+    shisha_resistance: 3
+    )
+  end
+  
+  Post.create!(
+    [
+      {
+        user_id: 1,
+        title: "オランジュ",
+        body: "吸いやすくて美味しいです。",
+        flavor_genre: 2,
+        player: 0,
+        location: "表参道",
+        hms_genre: 0,
+        top_genre: 0,
+        duration: 120,
+        price: 3000,
+        flavor_capacity: 12,
+        flavor_maker: "アルファーヘル",
+        smoking_level: 1,
+        smoking_taste_level: 1,
+        bottle_option: false,
+        nicotine: true,
+        star: 5
+      },
+       {
+        user_id: 2,
+        title: "パティサ",
+        body: "濃い！美味しいです。",
+        flavor_genre: 2,
+        player: 0,
+        location: "春日部",
+        hms_genre: 0,
+        top_genre: 0,
+        duration: 180,
+        price: 3000,
+        flavor_capacity: 12,
+        flavor_maker: "自家製",
+        smoking_level: 1,
+        smoking_taste_level: 1,
+        bottle_option: false,
+        nicotine: true,
+        star: 5
+      },
+       {
+        user_id: 3,
+        title: "キメト",
+        body: "ありえんほど美味しい。",
+        flavor_genre: 2,
+        player: 0,
+        location: "浅草",
+        hms_genre: 1,
+        top_genre: 0,
+        duration: 120,
+        price: 3000,
+        flavor_capacity: 12,
+        flavor_maker: "自家製",
+        smoking_level: 1,
+        smoking_taste_level: 1,
+        bottle_option: false,
+        nicotine: true,
+        star: 5
+      },
+       {
+        user_id: 1,
+        title: "オランジュ",
+        body: "吸いやすくて美味しいです。",
+        flavor_genre: 2,
+        player: 0,
+        location: "表参道",
+        hms_genre: 0,
+        top_genre: 0,
+        duration: 120,
+        price: 3000,
+        flavor_capacity: 12,
+        flavor_maker: "アルファーヘル",
+        smoking_level: 1,
+        smoking_taste_level: 1,
+        bottle_option: false,
+        nicotine: true,
+        star: 5
+      },
+       {
+        user_id: 4,
+        title: "オランジュ",
+        body: "美味しいです。",
+        flavor_genre: 2,
+        player: 0,
+        location: "表参道",
+        hms_genre: 0,
+        top_genre: 0,
+        duration: 100,
+        price: 3000,
+        flavor_capacity: 12,
+        flavor_maker: "ドザジ",
+        smoking_level: 1,
+        smoking_taste_level: 1,
+        bottle_option: false,
+        nicotine: true,
+        star: 4
+      },
+       {
+        user_id: 1,
+        title: "煙の国のアリス",
+        body: "閉店しました。。",
+        flavor_genre: 2,
+        player: 0,
+        location: "渋谷",
+        hms_genre: 0,
+        top_genre: 0,
+        duration: 120,
+        price: 3000,
+        flavor_capacity: 12,
+        flavor_maker: "ブタ",
+        smoking_level: 1,
+        smoking_taste_level: 1,
+        bottle_option: false,
+        nicotine: true,
+        star: 4
+      },
+       {
+        user_id: 5,
+        title: "ちるこれ",
+        body: "吸いやすくて美味しいです。",
+        flavor_genre: 2,
+        player: 0,
+        location: "表参道",
+        hms_genre: 0,
+        top_genre: 0,
+        duration: 120,
+        price: 3000,
+        flavor_capacity: 12,
+        flavor_maker: "アルファーヘル",
+        smoking_level: 1,
+        smoking_taste_level: 1,
+        bottle_option: false,
+        nicotine: true,
+        star: 5
+      },
+    ]
+  )
+  
+  
