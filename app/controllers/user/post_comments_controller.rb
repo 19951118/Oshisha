06557d318fc9c7fb07df14a0f20b9a35755bddb9
19951118 <reonn1118@yahoo.co.jp>
2023-12-27 05:31:16 +0000

    検証ツールからのアクセス制限(いいねコメントフォロー)

--- app/controllers/user/post_comments_controller.rb
+++ app/controllers/user/post_comments_controller.rb
@@ -1,18 +1,19 @@
 class User::PostCommentsController < ApplicationController
+  before_action :is_post_matching_login_user, only: [:destroy]
   def create
     @post = Post.find(params[:post_id])
     @comment = current_user.post_comments.new(post_comment_params)
     @comment.post_id = @post.id
     @comment.save
   end
-  
+
   def destroy
     @comment = PostComment.find(params[:id])
     PostComment.find(params[:id]).destroy
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
